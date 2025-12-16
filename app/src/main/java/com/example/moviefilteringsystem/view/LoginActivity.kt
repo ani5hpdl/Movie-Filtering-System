031ee0f1f45b0b6f827028c2d60cfd666cfe1870
itspirates635 <anishpoudel335@gmail.com>
2025-12-16 18:55:31 +0545

    Login Activity Updated

--- app/src/main/java/com/example/moviefilteringsystem/view/LoginActivity.kt
+++ app/src/main/java/com/example/moviefilteringsystem/view/LoginActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,6 +23,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -70,7 +75,7 @@ class LoginActivity : ComponentActivity() {
 @Composable
 fun LoginScreen() {
 
-//    val context = LocalContext.current
+    val context = LocalContext.current
 //    val activity = context as Activity
 
     var email by remember { mutableStateOf("") }
@@ -107,116 +112,147 @@ fun LoginScreen() {
                     color = Color.White
                 )
             )
-
-            OutlinedTextField(
-                value = email,
-                onValueChange = {
-                    email = it
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email
-                ),
-                placeholder = {
-                    Text("✉️ abc@gmail.com")
-                },
-                colors = TextFieldDefaults.colors(
-                    unfocusedContainerColor = Color.White,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedContainerColor = Color.White,
-                    focusedIndicatorColor = Color.Blue
-                ),
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp),
-                shape = RoundedCornerShape(15.dp)
-            )
+                    .padding(vertical = 40.dp)
+                    .background(Color.Black)
+            ) {
 
-            OutlinedTextField(
-                value = password,
-                onValueChange = { data ->
-                    password = data
-                },
-                placeholder = {
-                    Text("🔒 *********")
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        visibility = !visibility
-                    }) {
-                        Icon(
-                            painter = if (visibility)
-                                painterResource(R.drawable.baseline_visibility_24)
-                            else
-                                painterResource(R.drawable.baseline_visibility_off_24),
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    ),
+                    placeholder = {
+                        Text("abc@gmail.com",
+                            style = TextStyle(
+                                color = Color.White))
+                    },
+                    leadingIcon = { Icon(Icons.Default.Email, "Contact Number Icon") },
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Blue,
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        unfocusedLabelColor = Color.White
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .border(
+                            width = 2.dp,
+                            color = Color.White,
+                            shape = RoundedCornerShape(15.dp)
+                        ),
+                    shape = RoundedCornerShape(15.dp)
+                )
 
-                            contentDescription = null
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { data ->
+                        password = data
+                    },
+                    placeholder = {
+                        Text("*********",
+                            style = TextStyle(
+                                color = Color.White))
+                    },
+                    leadingIcon = { Icon(Icons.Default.Lock, "Contact Number Icon") },
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            visibility = !visibility
+                        }) {
+                            Icon(
+                                painter = if (visibility)
+                                    painterResource(R.drawable.baseline_visibility_24)
+                                else
+                                    painterResource(R.drawable.baseline_visibility_off_24),
+
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    visualTransformation = if (!visibility) PasswordVisualTransformation() else VisualTransformation.None,
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Blue,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        unfocusedLabelColor = Color.White
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .border(
+                            width = 2.dp,
+                            color = Color.White,
+                            shape = RoundedCornerShape(15.dp)
+                        ),
+                    shape = RoundedCornerShape(15.dp)
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    "Forget Password?",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 17.dp),
+                    style = TextStyle(
+                        color = Color.White,
+                        textAlign = TextAlign.End
+                    )
+                )
+
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp),
+                    shape = RoundedCornerShape(20.dp)
+                ) {
+                    Text(
+                        "Sign In",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold
                         )
-                    }
-                },
-                visualTransformation = if (!visibility) PasswordVisualTransformation() else VisualTransformation.None,
-                colors = TextFieldDefaults.colors(
-                    unfocusedContainerColor = Color.White,
-                    focusedContainerColor = Color.White,
-                    focusedIndicatorColor = Color.Blue,
-                    unfocusedIndicatorColor = Color.Transparent
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp),
-                shape = RoundedCornerShape(15.dp)
-            )
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Text(
-                "Forget Password?",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(end = 17.dp),
-                style = TextStyle(
-                    color = Color.White,
-                    textAlign = TextAlign.End
-                )
-            )
-
-            Button(
-                onClick = {
-
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-                shape = RoundedCornerShape(20.dp)
-            ) {
-                Text(
-                    "Sign In",
-                    style = TextStyle(
-                        fontWeight = FontWeight.Bold
                     )
-                )
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        "Don't have an account?",
+                        style = TextStyle(
+                            color = Color.White
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        "Sign Up",
+                        style = TextStyle(
+                            color = Purple40,
+                            fontWeight = FontWeight.ExtraBold
+                        ),
+                        modifier = Modifier.clickable {
+                            val intent = Intent(context, SignupActivity::class.java)
+                            context.startActivity(intent)
+                        }
+                    )
+                }
+
             }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    "Don't have an account?",
-                    style = TextStyle(
-                        color = Color.White
-                    )
-                )
-                Spacer(modifier = Modifier.width(10.dp))
-                Text(
-                    "Sign Up",
-                    style = TextStyle(
-                        color = Purple40,
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                )
-            }
-
-
         }
     }
 
