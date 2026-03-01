03f295b3c462d6bb66c0fdf195b517681297111c
ani5hpdl <178013051+ani5hpdl@users.noreply.github.com>
2026-03-01 18:23:34 +0545

    Updated Movie Repo acoording to Model

--- app/src/main/java/com/example/moviefilteringsystem/repository/MovieRepo.kt
+++ app/src/main/java/com/example/moviefilteringsystem/repository/MovieRepo.kt
@@ -1,5 +1,7 @@
 package com.example.moviefilteringsystem.repository
 
+import android.content.Context
+import android.net.Uri
 import com.example.moviefilteringsystem.model.MovieModel
 
 interface MovieRepo {
@@ -8,4 +10,6 @@ interface MovieRepo {
     fun getMovie(movieId: String, callback: (MovieModel?) -> Unit)
     fun updateMovie(movie: MovieModel, callback: (Boolean, String?) -> Unit)
     fun deleteMovie(movieId: String, callback: (Boolean, String?) -> Unit)
+    fun uploadImage(context: Context, imageUri: Uri, callback: (String?) -> Unit)
+    fun getFileNameFromUri(context: Context, uri: Uri): String?
 }
