ce3ea1793386f355ca75a6a8efc6673129384b84
ani5hpdl <178013051+ani5hpdl@users.noreply.github.com>
2026-03-02 13:11:28 +0545

    Updated UserView Model

--- app/src/main/java/com/example/moviefilteringsystem/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/moviefilteringsystem/viewmodel/UserViewModel.kt
@@ -3,76 +3,30 @@ package com.example.moviefilteringsystem.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.moviefilteringsystem.model.UserModel
 import com.example.moviefilteringsystem.repository.UserRepo
-import com.google.firebase.auth.FirebaseUser
 
-class UserViewModel(val repo: UserRepo) : ViewModel() {
-    fun login(
-        email: String,
-        password: String,
-        callback: (Boolean, String) -> Unit
-    ) {
-        repo.login(email, password, callback)
+class UserViewModel(private val repository: UserRepo) : ViewModel() {
+
+    fun createUser(userModel: UserModel, callback: (Boolean, String?) -> Unit) {
+        repository.createUser(userModel, callback)
     }
 
-    fun register(
-        email: String,
-        password: String,
-        callback: (Boolean,String,String) -> Unit
-    ) {
-        repo.register(email, password, callback)
+    fun login(email: String, password: String, callback: (Boolean, String?) -> Unit) {
+        repository.login(email, password, callback)
     }
 
-    fun forgetPassword(
-        email: String,
-        callback: (Boolean, String) -> Unit
-    ) {
-        repo.forgetPassword(email, callback)
+    fun getUsers(callback: (List<UserModel>?) -> Unit) {
+        repository.getUsers(callback)
     }
 
-    fun addUserToDatabase(
-        userId: String,
-        model: UserModel,
-        callback: (Boolean, String) -> Unit
-    ) {
-        repo.addUserToDatabase(userId, model, callback)
+    fun getUser(userId: String, callback: (UserModel?) -> Unit) {
+        repository.getUser(userId, callback)
     }
 
-    fun getUserById(
-        userId: String,
-        callback: (Boolean, UserModel?) -> Unit
-    ) {
-        repo.getUserById(userId, callback)
+    fun updateUser(user: UserModel, callback: (Boolean, String?) -> Unit) {
+        repository.updateUser(user, callback)
     }
 
-    fun getAllUser(
-        callback: (Boolean, List<UserModel>) -> Unit
-    ) {
-        repo.getAllUser(callback)
-    }
-
-    fun getcurrentUser(): FirebaseUser? {
-        return repo.getcurrentUser()
-    }
-
-    fun deleteUser(
-        userId: String,
-        callback: (Boolean, String) -> Unit
-    ) {
-        repo.deleteUser(userId, callback)
-    }
-
-    fun updateUser(
-        userId: String,
-        model: UserModel,
-        callback: (Boolean, String) -> Unit
-    ) {
-        repo.updateUser(userId, model, callback)
-    }
-
-    fun logout(
-        userId: String,
-        callback: (Boolean, String) -> Unit
-    ) {
-        repo.logout(userId, callback)
+    fun deleteUser(userId: String, callback: (Boolean, String?) -> Unit) {
+        repository.deleteUser(userId, callback)
     }
 }
