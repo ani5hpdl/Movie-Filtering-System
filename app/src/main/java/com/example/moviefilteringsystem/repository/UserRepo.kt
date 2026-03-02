f883c99bd0ed9ebbfc0558cd1982ba8ce7b1ea3f
ani5hpdl <178013051+ani5hpdl@users.noreply.github.com>
2026-03-02 13:07:35 +0545

    Updated User Repo

--- app/src/main/java/com/example/moviefilteringsystem/repository/UserRepo.kt
+++ app/src/main/java/com/example/moviefilteringsystem/repository/UserRepo.kt
@@ -1,49 +1,13 @@
 package com.example.moviefilteringsystem.repository
 
 import com.example.moviefilteringsystem.model.UserModel
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseUser
-import javax.security.auth.callback.Callback
 
 interface UserRepo {
-    fun login(email : String,
-              password : String,
-              callback: (Boolean, String) -> Unit
-    )
 
-    fun register(email : String,
-                 password : String,
-                 callback: (Boolean, String, String) -> Unit
-    )
-
-    fun forgetPassword(email: String,
-                       callback: (Boolean, String) -> Unit
-    )
-
-    fun addUserToDatabase(userId : String,
-                          model: UserModel,
-                          callback: (Boolean, String) -> Unit
-    )
-
-    fun getUserById(userId: String,
-                    callback: (Boolean, UserModel?) -> Unit
-    )
-
-    fun getAllUser(callback: (Boolean, List<UserModel>) -> Unit
-    )
-
-    fun getcurrentUser() : FirebaseUser?
-
-    fun deleteUser(userId: String,
-                   callback: (Boolean, String) -> Unit
-    )
-
-    fun updateUser(userId: String,
-                   model: UserModel,
-                   callback: (Boolean, String) -> Unit
-    )
-
-    fun logout(userId: String,
-               callback: (Boolean, String) -> Unit
-    )
+    fun createUser(userModel: UserModel, callback: (Boolean, String?) -> Unit)
+    fun login(email:String,password:String,callback: (Boolean, String?) -> Unit)
+    fun getUsers(callback: (List<UserModel>?) -> Unit)
+    fun getUser(userId: String, callback: (UserModel?) -> Unit)
+    fun updateUser(user: UserModel, callback: (Boolean, String?) -> Unit)
+    fun deleteUser(userId: String, callback: (Boolean, String?) -> Unit)
 }
