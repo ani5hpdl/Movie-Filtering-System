80ba0b27c068181d9fa4bdf4a953c502947ff667
ani5hpdl <178013051+ani5hpdl@users.noreply.github.com>
2026-03-02 13:07:10 +0545

    Updated Movie Repo Implemenatation

--- app/src/main/java/com/example/moviefilteringsystem/repository/MovieRepoImpl.kt
+++ app/src/main/java/com/example/moviefilteringsystem/repository/MovieRepoImpl.kt
@@ -1,13 +1,31 @@
 package com.example.moviefilteringsystem.repository
 
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import android.provider.OpenableColumns
+import com.cloudinary.Cloudinary
+import com.cloudinary.utils.ObjectUtils
 import com.example.moviefilteringsystem.model.MovieModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.io.InputStream
+import java.util.concurrent.Executors
 
 class MovieRepoImpl : MovieRepo {
     private val database = FirebaseDatabase.getInstance().getReference("Movies")
+
+    private val cloudinary = Cloudinary(
+        mapOf(
+            "cloud_name" to "dusnktkk7",
+            "api_key" to "597162626537592",
+            "api_secret" to "QCNxWNXYobaiZ9aO9Nm6AoPG-ME"
+        )
+    )
 
     override fun addMovie(movie: MovieModel, callback: (Boolean, String?) -> Unit) {
         val movieId = database.push().key ?: ""
@@ -60,5 +78,51 @@ class MovieRepoImpl : MovieRepo {
             .addOnFailureListener {
                 callback(false, it.message)
             }
+    }
+
+    override fun uploadImage(context: Context, imageUri: Uri, callback: (String?) -> Unit) {
+        val executor = Executors.newSingleThreadExecutor()
+        executor.execute {
+            try {
+                val inputStream: InputStream? = context.contentResolver.openInputStream(imageUri)
+                var fileName = getFileNameFromUri(context, imageUri)
+
+                fileName = fileName?.substringBeforeLast(".") ?: "uploaded_movie_image"
+
+                val response = cloudinary.uploader().upload(
+                    inputStream, ObjectUtils.asMap(
+                        "public_id", fileName,
+                        "resource_type", "image"
+                    )
+                )
+
+                var imageUrl = response["url"] as String?
+                imageUrl = imageUrl?.replace("http://", "https://")
+
+                Handler(Looper.getMainLooper()).post {
+                    callback(imageUrl)
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Handler(Looper.getMainLooper()).post {
+                    callback(null)
+                }
+            }
+        }
+    }
+
+    override fun getFileNameFromUri(context: Context, uri: Uri): String? {
+        var fileName: String? = null
+        val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (nameIndex != -1) {
+                    fileName = it.getString(nameIndex)
+                }
+            }
+        }
+        return fileName
     }
 }
